eb08ab62f5f3b1d3d1260b809272d05d4b514d5c
David Gibson <dgibson314@gmail.com>
2023-11-02 08:04:49 -0400

    initial

--- src/lox_value.py
+++ src/lox_value.py
@@ -35,3 +35,10 @@ class Value():
     def is_falsey(self):
         return self.value is None or self.value is False
 
+
+class Function():
+    def __init__(self, obj, arity, chunk, name):
+        self.obj = obj
+        self.arity = arity
+        self.chunk = chunk
+        self.name = name
